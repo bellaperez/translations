418f5a44406a6502df03f382d08d8a6ff1d8154d
Antoni Bella Pérez <antonibella5@yahoo.com>
2019-03-10 21:50:08 +0100

    Second Catalan translation of CPU X

--- CPU_X/xmlpo.sh
+++ CPU_X/xmlpo.sh
@@ -1,25 +1,55 @@
 #!/bin/sh
 
-# Create POT template file
-# itstool --out=cpu_x.pot translatestrings.xml 
-# Bé
+LANG="$2"
 
-# Merge my old XML translations
-# Manually
+message_output() {
+    echo "$0 [ make-pot | update-po | make-xml | make-bz2 ] [ ca | es ]"
+    echo
+    echo "  # Look before ─ when updating do it while translating (into cpu_x_[ ca | es ].po file):"
+    echo "      \\\n -> "'\\'"\\\n"
+    echo "       ' -> "'\\'"\\'"
+    echo
+    echo "  # Look after (into strings_[ ca | es ].xml file):"
+    echo "      <![CDATA[ ... ]]>"
+    echo
+    exit 0
+  }
 
-# Update PO file
-# msgmerge --update cpu_x_ca.po cpu_x.pot
-# Bé
+language_id() {
+    case $1 in
+      ca|es)
+        echo
+      ;;
+      *)
+        message_output
+      ;;
+    esac
+  }
 
-# Mirar abans (en actualitzar fer-ho mentre es tradueix):
-# \n -> \\n
-# '  -> \\'
-# Create my XML translation
-msgfmt  -o cpu_x_ca.mo cpu_x_ca.po
-itstool -m cpu_x_ca.mo -o strings_ca.xml translatestrings.xml
-
-# Mirar d'esprés:
-# <![CDATA[Ajuda i política]]>
+case $1 in
+  make-pot)
+    language_id $2
+    # Create POT template file
+    itstool --out=cpu_x.pot --strict translatestrings.xml 
+  ;;
+  update-po)
+    language_id $2
+    # Update PO file
+    msgmerge --update cpu_x_$LANG.po cpu_x.pot
+  ;;
+  make-xml)
+    language_id $2
+    # Create XML translation
+    msgfmt  -o cpu_x_$LANG.mo cpu_x_$LANG.po
+    itstool -m cpu_x_$LANG.mo -o strings_$LANG.xml translatestrings.xml
+  ;;
+  make-bz2)
+    tar -cvjf Catalan-Spanish.tbz2 strings_*
+  ;;
+  *)
+    message_output
+  ;;
+esac
 
 exit 0
 
